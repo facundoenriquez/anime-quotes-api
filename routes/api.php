3961a864f513406b14b67f0ecb9db27f68dd97b7
Facundo Enriquez <fenriquez@inym.org.ar>
2024-06-10 13:53:44 -0300

    3rd push

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\AnimeController;
+use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('anime', AnimeController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
+Route::resource('character', CharacterController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
+Route::resource('quote', QuoteController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
