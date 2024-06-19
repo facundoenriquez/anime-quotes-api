9301ed44c315f054e1408959e8ea2c95ba02f8f4
Facundo Enriquez <fenriquez@inym.org.ar>
2024-06-19 13:54:27 -0300

    posts and comments

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +18,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::resource('anime', AnimeController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
 Route::resource('character', CharacterController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
 Route::resource('quote', QuoteController::class)->except(['create', 'store', "edit", 'update', 'destroy']);
+
+// POSTS AND COMMENTS
+Route::resource('post', PostController::class)->middleware('auth:sanctum');
+Route::resource('comment', CommentController::class)->middleware('auth:sanctum');
